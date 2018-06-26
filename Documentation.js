de0ab2d96019d043f9be012cfa4447b6891e862a
Nick Dugger <email@nickdugger.com>
2018-06-26 13:49:21 -0500

    Update Documentation.js

--- Documentation.js
+++ Documentation.js
@@ -1,5 +1,41 @@
 class Speficitaion {
     
+    constructor (documentation) {
+        const { of, require } = documentation;
+        
+        this.documentation = documentation;
+        this.markdown = `
+            # ${ of.name }
+            ${ require ? `\`const ${ of.name } = require('${ require }');\`` : '' }
+        `;
+    }
+    
+    extends (parent) {
+        const { of } = this.documentation;
+        const regexp = new RegExp(`# (${ of.name })`);
+        
+        this.markdown.replace(regexp, of => `${ of } : ${ parent.name }`);
+    }
+    
+    field (name, type, description) {
+        const { of } = this.documentation;
+        const self = of.name.toLowerCase();
+        
+        this.markdown += `
+            #### \`${ type.name } ${ self }.${ name }\`
+            ${ description }
+        `;
+    }
+    
+    method (name, args, type, description) {
+        const { of } = this.documentation;
+        const self = of.name.toLowerCase();
+        
+        this.markdown += `
+            #### \`${ type.name } ${ self }.${ name }(${ args.join(', ') })\`
+            ${ description }
+        `;
+    }
 }
 
 module.exports = class Documentation {
@@ -11,6 +47,10 @@ module.exports = class Documentation {
     }
     
     generate () {
-        return this.specification(new Specification(this));
+        const specification = new Specification(this);
+        
+        this.specification(specification);
+        
+        return specification.markdown;
     }
 };
