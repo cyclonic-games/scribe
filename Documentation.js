46c527a214983f00800f628ebc8b651d4eff0efa
Nick Dugger <email@nickdugger.com>
2018-06-26 13:01:57 -0500

    Update Documentation.js

--- Documentation.js
+++ Documentation.js
@@ -1,1 +1,16 @@
+class Speficitaion {
+    
+}
 
+module.exports = class Documentation {
+    
+    constructor (of, require, specification) {
+        this.of = of;
+        this.require = require;
+        this.specification = specification;
+    }
+    
+    generate () {
+        return this.specification(new Specification(this));
+    }
+};
