8de0f6de214b7182196fbccd84bf43933c26a5e4
Nick Dugger <email@nickdugger.com>
2018-06-26 16:21:21 -0500

    Update Documentation.js

--- Documentation.js
+++ Documentation.js
@@ -1,15 +1,15 @@
 const requires = new Map();
 
-class Speficitaion {
+class Specification {
     
     constructor (documentation) {
         const { of, require } = documentation;
         
         this.documentation = documentation;
-        this.markdown = `
+        this.markdown = this.documentation.outdent(`
             # ${ of.name }
             ${ require ? `\`const ${ of.name } = require('${ require }');\`` : '' }
-        `;
+        `);
     }
     
     extends (parent) {
@@ -23,28 +23,28 @@ class Speficitaion {
         const { of } = this.documentation;
         const self = of.name.toLowerCase();
         
-        this.markdown += `
+        this.markdown += this.documentation.outdent(`
             - #### *\`${ self }.${ name } -> ${ type.name }\`*
-              ${ description }
-        `;
+              ${ this.documentation.outdent(description) }
+        `);
     }
     
     method (name, args, type, description) {
         const { of } = this.documentation;
         const self = of.name.toLowerCase();
         
-        this.markdown += `
+        this.markdown += this.documentation.outdent(`
             - #### *\`${ self }.${ name }(${ args.join(', ') }) -> ${ type.name }\`*
-              ${ description }
-        `;
+              ${ this.documentation.outdent(description) }
+        `);
     }
     
     section (title, content) {
-        this.markdown += `
+        this.markdown += this.documentation.outdent(`
             ---
             ## ${ title }
-            ${ content }
-        `;
+            ${ this.documentation.outdent(content) }
+        `);
     }
 }
 
@@ -58,11 +58,18 @@ module.exports = class Documentation {
         requires.set(of, require);
     }
     
+    outdent (markdown) {
+        const [ , indentation ] = markdown.match(/^\n(\s+?)(?=\S)/) || [ '' ];
+        const outdented = markdown.replace(new RegExp(`\n${ indentation }`, 'g'), '\n');
+        
+        return outdented;
+    }
+    
     generate () {
         const specification = new Specification(this);
         
         this.specification(specification);
         
-        return specification.markdown;
+        return this.outdent(specification.markdown);
     }
 };
